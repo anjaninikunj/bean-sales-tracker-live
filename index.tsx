e5861ef7f02b7105857888d117841a74ee6154c5
anjanipatel <anjaninikunj@gmail.com>
2026-01-10 13:26:24 +0530

    Fix black screen and add deployment config Antigravity Help

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+
 
 const mountApp = () => {
   const rootElement = document.getElementById('root');
